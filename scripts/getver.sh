a29f493b8008f3a80ee97b458244a226106ffb59
Huangbin Zhan <zhanhb88@gmail.com>
2021-01-02 05:50:55 +0800

    scripts: getver.sh: remove UPSTREAM_REV suffix
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -26,7 +26,7 @@ try_git() {
 		[ $((BASE_REV - GET_REV)) -ge 0 ] && REV="$(git rev-parse HEAD~$((BASE_REV - GET_REV)))"
 		;;
 	*)
-		local BRANCH ORIGIN UPSTREAM_BASE UPSTREAM_REV
+		local BRANCH ORIGIN UPSTREAM_BASE
 		BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 		ORIGIN="$(git rev-parse --verify --symbolic-full-name "$BRANCH@{u}" 2>/dev/null)"
 		[ -n "$ORIGIN" ] || ORIGIN="$(git rev-parse --verify --symbolic-full-name "master@{u}" 2>/dev/null)"
@@ -34,13 +34,6 @@ try_git() {
 
 		if [ -n "$ORIGIN" ]; then
 			UPSTREAM_BASE="$(git merge-base "$GET_REV" "$ORIGIN")"
-			UPSTREAM_REV="$(git rev-list "${REBOOT}..$UPSTREAM_BASE" 2>/dev/null | wc -l | awk '{print $1}')"
-		else
-			UPSTREAM_REV=0
-		fi
-
-		if [ "$REV" -gt "$UPSTREAM_REV" ]; then
-			REV="${UPSTREAM_REV}+$((REV - UPSTREAM_REV))"
 		fi
 
 		REV="${REV:+r$REV-$(git log -n 1 --format="%h" "$UPSTREAM_BASE")}"
