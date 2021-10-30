00c31f2ee5ed145f7f74fc587b7cd9c405d6895e
Huangbin Zhan <zhanhb88@gmail.com>
2021-10-31 03:07:54 +0800

    scripts: clean gen-dependencies.sh
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- scripts/gen-dependencies.sh
+++ scripts/gen-dependencies.sh
@@ -5,29 +5,28 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
-SELF=${0##*/}
+SELF="${0##*/}"
 
 READELF="${READELF:-readelf}"
 OBJCOPY="${OBJCOPY:-objcopy}"
-TARGETS=$*
 XARGS="${XARGS:-xargs -r}"
 
-[ -z "$TARGETS" ] && {
+[ "$#" -gt 0 ] || {
   echo "$SELF: no directories / files specified"
   echo "usage: $SELF [PATH...]"
   exit 1
 }
 
-find $TARGETS -type f -a -exec file {} \; | \
-  sed -n -e 's/^\(.*\):.*ELF.*\(executable\|shared object\).*,.*/\1/p' | \
-  $XARGS -n1 $READELF -d | \
-  awk '$2 ~ /NEEDED/ && $NF !~ /interpreter/ && $NF ~ /^\[?lib.*\.so/ { gsub(/[\[\]]/, "", $NF); print $NF }' | \
+find "$@" -type f -exec file {} \; |
+  sed -n -e 's/^\(.*\):.*ELF.*\(executable\|shared object\).*,.*/\1/p' |
+  $XARGS -n1 $READELF -d |
+  awk '$2 ~ /NEEDED/ && $NF !~ /interpreter/ && $NF ~ /^\[?lib.*\.so/ { gsub(/[\[\]]/, "", $NF); print $NF }' |
   sort -u
 
-tmp=$(mktemp $TMP_DIR/dep.XXXXXXXX)
-for kmod in $(find $TARGETS -type f -name \*.ko); do
-	$OBJCOPY -O binary -j .modinfo $kmod $tmp
-	sed -e 's,\x00,\n,g' $tmp | \
+tmp="$(mktemp "$TMP_DIR/dep.XXXXXXXX")"
+find "$@" -type f -name \*.ko | while read -r kmod; do
+	$OBJCOPY -O binary -j .modinfo "$kmod" "$tmp"
+	sed -e 's,\x00,\n,g' "$tmp" |
 		sed -ne '/^depends=.\+/ { s/^depends=//; s/,/.ko\n/g; s/$/.ko/p; q }'
 done | sort -u
-rm -f $tmp
+rm -f "$tmp"
