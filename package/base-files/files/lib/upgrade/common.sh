d89c5948fe0dc23615851bec0e263a741c058d6d
Huangbin Zhan <zhanhb88@gmail.com>
2021-11-02 04:55:48 +0800

    base-files: clean common.sh
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -1,17 +1,18 @@
+# shellcheck disable=SC3043,SC3057,SC3060,SC3003
 RAM_ROOT=/tmp/root
 
 export BACKUP_FILE=sysupgrade.tgz	# file extracted by preinit
 
-[ -x /usr/bin/ldd ] || ldd() { LD_TRACE_LOADED_OBJECTS=1 $*; }
-libs() { ldd $* 2>/dev/null | sed -E 's/(.* => )?(.*) .*/\2/'; }
+[ -x /usr/bin/ldd ] || ldd() { LD_TRACE_LOADED_OBJECTS=1 "$@"; }
+libs() { ldd "$@" 2>/dev/null | sed 's#^[[:space:]]\(.* => \)\{0,1\}\(/.*\) ([^()]*)$#\2#p;d'; }
 
 install_file() { # <file> [ <file> ... ]
-	local target dest dir
+	local file target dest dir
 	for file in "$@"; do
 		if [ -L "$file" ]; then
 			target="$(readlink -f "$file")"
 			dest="$RAM_ROOT/$file"
-			[ ! -f "$dest" ] && {
+			[ -f "$dest" ] || {
 				dir="$(dirname "$dest")"
 				mkdir -p "$dir"
 				ln -s "$target" "$dest"
@@ -19,7 +20,7 @@ install_file() { # <file> [ <file> ... ]
 			file="$target"
 		fi
 		dest="$RAM_ROOT/$file"
-		[ -f "$file" -a ! -f "$dest" ] && {
+		[ -f "$file" ] && [ ! -f "$dest" ] && {
 			dir="$(dirname "$dest")"
 			mkdir -p "$dir"
 			cp "$file" "$dest"
@@ -28,11 +29,13 @@ install_file() { # <file> [ <file> ... ]
 }
 
 install_bin() {
-	local src files
-	src=$1
-	files=$1
-	[ -x "$src" ] && files="$src $(libs $src)"
-	install_file $files
+	local src="$1" file
+	install_file "$src"
+	if [ -x "$src" ]; then
+		libs "$src" | while read -r file; do
+			install_file "$file"
+		done
+	fi
 }
 
 run_hooks() {
@@ -48,13 +51,13 @@ ask_bool() {
 
 	[ "$INTERACTIVE" -eq 1 ] && {
 		case "$default" in
-			0) echo -n "$* (y/N): ";;
-			*) echo -n "$* (Y/n): ";;
+			0) printf "%s (y/N): " "$*";;
+			*) printf "%s (Y/n): " "$*";;
 		esac
-		read answer
+		read -r answer
 		case "$answer" in
-			y*) answer=1;;
-			n*) answer=0;;
+			Y* | y*) answer=1;;
+			N* | n*) answer=0;;
 			*) answer="$default";;
 		esac
 	}
@@ -66,7 +69,7 @@ _v() {
 }
 
 v() {
-	_v "$(date) upgrade: $@"
+	_v "$(date) upgrade: $*"
 	logger -p info -t upgrade "$@"
 }
 
@@ -86,8 +89,7 @@ get_image() { # <source> [ <command> ]
 	local cmd="$2"
 
 	if [ -z "$cmd" ]; then
-		local magic="$(dd if="$from" bs=2 count=1 2>/dev/null | hexdump -n 2 -e '1/1 "%02x"')"
-		case "$magic" in
+		case "$(hexdump -n 2 -e '1/1 "%02x"' "$from")" in
 			1f8b) cmd="busybox zcat";;
 			*) cmd="cat";;
 		esac
@@ -101,18 +103,18 @@ get_image_dd() {
 
 	(
 		exec 3>&2
-		( exec 3>&2; get_image "$from" 2>&1 1>&3 | grep -v -F ' Broken pipe'     ) 2>&1 1>&3 \
-			| ( exec 3>&2; dd "$@" 2>&1 1>&3 | grep -v -E ' records (in|out)') 2>&1 1>&3
+		( exec 3>&2; get_image "$from" 2>&1 1>&3 | grep -v -F ' Broken pipe'     ) 2>&1 1>&3 |
+			( exec 3>&2; dd "$@" 2>&1 1>&3 | grep -v -E ' records (in|out)') 2>&1 1>&3
 		exec 3>&-
 	)
 }
 
 get_magic_word() {
-	(get_image "$@" | dd bs=2 count=1 | hexdump -v -n 2 -e '1/1 "%02x"') 2>/dev/null
+	(get_image "$@" | hexdump -v -n 2 -e '1/1 "%02x"')
 }
 
 get_magic_long() {
-	(get_image "$@" | dd bs=4 count=1 | hexdump -v -n 4 -e '1/1 "%02x"') 2>/dev/null
+	(get_image "$@" | hexdump -v -n 4 -e '1/1 "%02x"')
 }
 
 get_magic_gpt() {
@@ -155,19 +157,16 @@ identify_magic_long() {
 }
 
 part_magic_efi() {
-	local magic=$(get_magic_gpt "$@")
-	[ "$magic" = "EFI PART" ]
+	[ "$(get_magic_gpt "$@")" = "EFI PART" ]
 }
 
 part_magic_fat() {
-	local magic=$(get_magic_vfat "$@")
-	local magic_fat32=$(get_magic_fat32 "$@")
-	[ "$magic" = "FAT" ] || [ "$magic_fat32" = "FAT32" ]
+	[ "$(get_magic_vfat "$@")" = FAT ] || [ "$(get_magic_fat32 "$@")" = FAT32 ]
 }
 
 export_bootdevice() {
-	local cmdline uuid blockdev uevent line class
-	local MAJOR MINOR DEVNAME DEVTYPE
+	local uuid blockdev uevent line class
+	local MAJOR MINOR DEVNAME
 	local rootpart="$(cmdline_get_var root)"
 
 	case "$rootpart" in
@@ -175,7 +174,7 @@ export_bootdevice() {
 			uuid="${rootpart#PARTUUID=}"
 			uuid="${uuid%-[a-f0-9][a-f0-9]}"
 			for blockdev in $(find /dev -type b); do
-				set -- $(dd if=$blockdev bs=1 skip=440 count=4 2>/dev/null | hexdump -v -e '4/1 "%02x "')
+				set -- $(hexdump -v -s 440 -n 4 -e '4/1 "%02x "' "$blockdev")
 				if [ "$4$3$2$1" = "$uuid" ]; then
 					uevent="/sys/class/block/${blockdev##*/}/uevent"
 					break
@@ -188,7 +187,7 @@ export_bootdevice() {
 			uuid="${uuid%/PARTNROFF=1}"
 			uuid="${uuid%0?}00"
 			for disk in $(find /dev -type b); do
-				set -- $(dd if=$disk bs=1 skip=568 count=16 2>/dev/null | hexdump -v -e '8/1 "%02x "" "2/1 "%02x""-"6/1 "%02x"')
+				set -- $(hexdump -v -s 568 -n 16 -e '8/1 "%02x "" "2/1 "%02x""-"6/1 "%02x"' "$disk")
 				if [ "$4$3$2$1-$6$5-$8$7-$9" = "$uuid" ]; then
 					uevent="/sys/class/block/${disk##*/}/uevent"
 					break
@@ -200,12 +199,12 @@ export_bootdevice() {
 		;;
 		0x[a-f0-9][a-f0-9][a-f0-9] | 0x[a-f0-9][a-f0-9][a-f0-9][a-f0-9] | \
 		[a-f0-9][a-f0-9][a-f0-9] | [a-f0-9][a-f0-9][a-f0-9][a-f0-9])
-			rootpart=0x${rootpart#0x}
+			rootpart="0x${rootpart#0x}"
 			for class in /sys/class/block/*; do
-				while read line; do
+				while read -r line; do
 					export -n "$line"
 				done < "$class/uevent"
-				if [ $((rootpart/256)) = $MAJOR -a $((rootpart%256)) = $MINOR ]; then
+				if [ $((rootpart/256)) = "$MAJOR" ] && [ $((rootpart%256)) = "$MINOR" ]; then
 					uevent="$class/../uevent"
 				fi
 			done
@@ -213,11 +212,11 @@ export_bootdevice() {
 	esac
 
 	if [ -e "$uevent" ]; then
-		while read line; do
+		while read -r line; do
 			export -n "$line"
 		done < "$uevent"
-		export BOOTDEV_MAJOR=$MAJOR
-		export BOOTDEV_MINOR=$MINOR
+		export "BOOTDEV_MAJOR=$MAJOR"
+		export "BOOTDEV_MINOR=$MINOR"
 		return 0
 	fi
 
@@ -226,13 +225,13 @@ export_bootdevice() {
 
 export_partdevice() {
 	local var="$1" offset="$2"
-	local uevent line MAJOR MINOR DEVNAME DEVTYPE
+	local uevent line MAJOR MINOR DEVNAME
 
 	for uevent in /sys/class/block/*/uevent; do
-		while read line; do
+		while read -r line; do
 			export -n "$line"
 		done < "$uevent"
-		if [ "$BOOTDEV_MAJOR" = "$MAJOR" -a $(($BOOTDEV_MINOR + $offset)) = "$MINOR" -a -b "/dev/$DEVNAME" ]; then
+		if [ "$BOOTDEV_MAJOR" = "$MAJOR" ] && [ "$((BOOTDEV_MINOR + offset))" = "$MINOR" ] && [ -b "/dev/$DEVNAME" ]; then
 			export "$var=$DEVNAME"
 			return 0
 		fi
@@ -241,58 +240,47 @@ export_partdevice() {
 	return 1
 }
 
-hex_le32_to_cpu() {
-	[ "$(echo 01 | hexdump -v -n 2 -e '/2 "%x"')" = "3031" ] && {
-		echo "${1:0:2}${1:8:2}${1:6:2}${1:4:2}${1:2:2}"
-		return
-	}
-	echo "$@"
-}
-
 get_partitions() { # <device> <filename>
 	local disk="$1"
 	local filename="$2"
 
-	if [ -b "$disk" -o -f "$disk" ]; then
+	if [ -b "$disk" ] || [ -f "$disk" ]; then
 		v "Reading partition table from $filename..."
 
-		local magic=$(dd if="$disk" bs=2 count=1 skip=255 2>/dev/null)
-		if [ "$magic" != $'\x55\xAA' ]; then
+		[ "$(hexdump -v -s 510 -n 2 -e '1/1 "%02x"' "$disk")" = 55aa ] || {
 			v "Invalid partition table on $disk"
 			exit
-		fi
+		}
 
 		rm -f "/tmp/partmap.$filename"
 
 		local part
-		part_magic_efi "$disk" && {
+		if part_magic_efi "$disk"; then
 			#export_partdevice will fail when partition number is greater than 15, as
 			#the partition major device number is not equal to the disk major device number
 			for part in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-				set -- $(hexdump -v -n 48 -s "$((0x380 + $part * 0x80))" -e '4/4 "%08x"" "4/4 "%08x"" "4/4 "0x%08X "' "$disk")
+				set -- $(hexdump -v -n 48 -s "$((0x380 + part * 0x80))" -e '4/4 "%08x"" "4/4 "%08x"" "16/1 " %02x"' "$disk")
 
 				local type="$1"
-				local lba="$(( $(hex_le32_to_cpu $4) * 0x100000000 + $(hex_le32_to_cpu $3) ))"
-				local end="$(( $(hex_le32_to_cpu $6) * 0x100000000 + $(hex_le32_to_cpu $5) ))"
-				local num="$(( $end - $lba + 1 ))"
+				local lba="$(( 0x${10}$9$8$7$6$5$4$3 ))"
+				local end="$(( 0x${18}${17}${16}${15}${14}${13}${12}${11} ))"
+				local num="$(( end - lba + 1 ))"
 
-				[ "$type" = "00000000000000000000000000000000" ] && continue
-
-				printf "%2d %5d %7d\n" $part $lba $num >> "/tmp/partmap.$filename"
+				[ "$type" = 00000000000000000000000000000000 ] || printf "%2d %5d %7d\n" "$part" "$lba" "$num" >> "/tmp/partmap.$filename"
 			done
-		} || {
+		else
 			for part in 1 2 3 4; do
-				set -- $(hexdump -v -n 12 -s "$((0x1B2 + $part * 16))" -e '3/4 "0x%08X "' "$disk")
+				set -- $(hexdump -v -n 12 -s "$((0x1B2 + part * 16))" -e '12/1 "%02x "' "$disk")
 
-				local type="$(( $(hex_le32_to_cpu $1) % 256))"
-				local lba="$(( $(hex_le32_to_cpu $2) ))"
-				local num="$(( $(hex_le32_to_cpu $3) ))"
+				local type="$(( 0x$1 ))"
+				local lba="$(( 0x$8$7$6$5 ))"
+				local num="$(( 0x${12}${11}${10}$9 ))"
 
 				[ $type -gt 0 ] || continue
 
-				printf "%2d %5d %7d\n" $part $lba $num >> "/tmp/partmap.$filename"
+				printf "%2d %5d %7d\n" "$part" "$lba" "$num" >> "/tmp/partmap.$filename"
 			done
-		}
+		fi
 	fi
 }
 
@@ -312,6 +300,6 @@ default_do_upgrade() {
 		get_image "$1" "$2" | mtd $MTD_ARGS $MTD_CONFIG_ARGS -j "$UPGRADE_BACKUP" write - "${PART_NAME:-image}"
 	else
 		get_image "$1" "$2" | mtd $MTD_ARGS write - "${PART_NAME:-image}"
-	fi
-	[ $? -ne 0 ] && exit 1
+	fi || exit 1
+	false
 }
