8c17cbf2e396bba03f3a9b1799f0e1525ae8a9cb
Huangbin Zhan <zhanhb88@gmail.com>
2021-11-07 19:58:56 +0800

    ath79: change sysupgrade image extension to tar
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -64,10 +64,10 @@ define Device/aerohive_hiveap-121
   KERNEL_SIZE := 5120k
   UBINIZE_OPTS := -E 5
   SUPPORTED_DEVICES += hiveap-121
-  IMAGES += factory.bin
+  IMAGES := factory.bin sysupgrade.tar
   IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | \
 	check-size
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += aerohive_hiveap-121
 
@@ -82,8 +82,8 @@ define Device/domywifi_dw33d
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   UBINIZE_OPTS := -E 5
-  IMAGES += factory.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGES := factory.bin sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | \
 	check-size
 endef
@@ -102,10 +102,10 @@ define Device/dongwon_dw02-412h
   KERNEL := $$(KERNEL) | dongwon-header
   KERNEL_INITRAMFS := $$(KERNEL)
   UBINIZE_OPTS := -E 5
-  IMAGES += factory.img
+  IMAGES := factory.img sysupgrade.tar
   IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | \
 	check-size
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
 endef
 
 define Device/dongwon_dw02-412h-64m
@@ -139,9 +139,9 @@ define Device/glinet_gl-ar300m-nand
   $(Device/glinet_gl-ar300m-common-nand)
   DEVICE_VARIANT := NAND
   BLOCKSIZE := 128k
-  IMAGES += factory.img
+  IMAGES := factory.img sysupgrade.tar
   IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
   SUPPORTED_DEVICES += glinet,gl-ar300m-nor
 endef
 TARGET_DEVICES += glinet_gl-ar300m-nand
@@ -166,7 +166,8 @@ define Device/glinet_gl-ar750s-nor-nand
   $(Device/glinet_gl-ar750s-common)
   DEVICE_VARIANT := NOR/NAND
   KERNEL_SIZE := 4096k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGES := sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
   SUPPORTED_DEVICES += glinet,gl-ar750s-nor
 endef
 TARGET_DEVICES += glinet_gl-ar750s-nor-nand
@@ -189,9 +190,9 @@ define Device/glinet_gl-e750
   PAGESIZE := 2048
   VID_HDR_OFFSET := 2048
   BLOCKSIZE := 128k
-  IMAGES += factory.img
+  IMAGES := factory.img sysupgrade.tar
   IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += glinet_gl-e750
 
@@ -205,9 +206,9 @@ define Device/glinet_gl-xe300
   PAGESIZE := 2048
   VID_HDR_OFFSET := 2048
   BLOCKSIZE := 128k
-  IMAGES += factory.img
+  IMAGES := factory.img sysupgrade.tar
   IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += glinet_gl-xe300
 
@@ -224,10 +225,10 @@ define Device/netgear_ath79_nand
 	append-string -e '\xff' | \
 	append-uImage-fakehdr filesystem $$(UIMAGE_MAGIC)
   KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | uImage lzma
-  IMAGES := sysupgrade.bin factory.img
+  IMAGES := factory.img sysupgrade.tar
   IMAGE/factory.img := append-kernel | append-ubi | netgear-dni | \
 	check-size
-  IMAGE/sysupgrade.bin := sysupgrade-tar | check-size | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | check-size | append-metadata
   UBINIZE_OPTS := -E 5
 endef
 
@@ -312,7 +313,8 @@ define Device/zte_mf286_common
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGES := sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
 endef
 
 define Device/zte_mf286
