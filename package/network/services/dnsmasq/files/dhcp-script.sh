cba9b5414b30dec3974db01696f14500378d237e
Huangbin Zhan <zhanhb88@gmail.com>
2021-11-21 13:57:30 +0800

    dnsmasq: clean scripts
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- package/network/services/dnsmasq/files/dhcp-script.sh
+++ package/network/services/dnsmasq/files/dhcp-script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# shellcheck disable=SC1090,SC2240
 [ -f "$USER_DHCPSCRIPT" ] && . "$USER_DHCPSCRIPT" "$@"
 
 . /usr/share/libubox/jshn.sh
 
 json_init
 json_add_array env
-hotplugobj=""
+hotplugobj=
 
 case "$1" in
 	add | del | old | arp-add | arp-del)
@@ -17,37 +18,37 @@ esac
 
 case "$1" in
 	add)
-		json_add_string "" "ACTION=add"
+		json_add_string "" ACTION=add
 		json_add_string "" "HOSTNAME=$4"
-		hotplugobj="dhcp"
+		hotplugobj=dhcp
 	;;
 	del)
-		json_add_string "" "ACTION=remove"
+		json_add_string "" ACTION=remove
 		json_add_string "" "HOSTNAME=$4"
-		hotplugobj="dhcp"
+		hotplugobj=dhcp
 	;;
 	old)
-		json_add_string "" "ACTION=update"
+		json_add_string "" ACTION=update
 		json_add_string "" "HOSTNAME=$4"
-		hotplugobj="dhcp"
+		hotplugobj=dhcp
 	;;
 	arp-add)
-		json_add_string "" "ACTION=add"
-		hotplugobj="neigh"
+		json_add_string "" ACTION=add
+		hotplugobj=neigh
 	;;
 	arp-del)
-		json_add_string "" "ACTION=remove"
-		hotplugobj="neigh"
+		json_add_string "" ACTION=remove
+		hotplugobj=neigh
 	;;
 	tftp)
-		json_add_string "" "ACTION=add"
+		json_add_string "" ACTION=add
 		json_add_string "" "TFTP_SIZE=$2"
 		json_add_string "" "TFTP_ADDR=$3"
 		json_add_string "" "TFTP_PATH=$4"
-		hotplugobj="tftp"
+		hotplugobj=tftp
 	;;
 esac
 
 json_close_array env
 
-[ -n "$hotplugobj" ] && ubus call hotplug.${hotplugobj} call "$(json_dump)"
+[ -z "${hotplugobj-}" ] || ubus call "hotplug.$hotplugobj" call "$(json_dump)"
