33af777ca2d9613e5177e4e670d478e0ed7a4c9a
Huangbin Zhan <zhanhb88@gmail.com>
2022-02-26 23:43:55 +0800

    scripts: clean diffconfig.sh
    Signed-off-by: Huangbin Zhan <zhanhb88@gmail.com>

--- scripts/diffconfig.sh
+++ scripts/diffconfig.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 make ./scripts/config/conf >/dev/null || { make ./scripts/config/conf; exit 1; }
-grep \^CONFIG_TARGET_ .config | head -n3 > tmp/.diffconfig.head
-grep \^CONFIG_TARGET_DEVICE_ .config >> tmp/.diffconfig.head
-grep '^CONFIG_ALL=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_ALL_KMODS=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_ALL_NONSHARED=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_DEVEL=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_TOOLCHAINOPTS=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_BUSYBOX_CUSTOM=y' .config >> tmp/.diffconfig.head
-grep '^CONFIG_TARGET_PER_DEVICE_ROOTFS=y' .config >> tmp/.diffconfig.head
+{
+	grep '^CONFIG_TARGET_' .config | head -n3
+	grep '^CONFIG_TARGET_DEVICE_' .config
+	grep '^CONFIG_ALL=y' .config
+	grep '^CONFIG_ALL_KMODS=y' .config
+	grep '^CONFIG_ALL_NONSHARED=y' .config
+	grep '^CONFIG_DEVEL=y' .config
+	grep '^CONFIG_TOOLCHAINOPTS=y' .config
+	grep '^CONFIG_BUSYBOX_CUSTOM=y' .config
+	grep '^CONFIG_TARGET_PER_DEVICE_ROOTFS=y' .config
+} >tmp/.diffconfig.head
 ./scripts/config/conf --defconfig=tmp/.diffconfig.head -w tmp/.diffconfig.stage1 Config.in >/dev/null
 ./scripts/kconfig.pl '>+' tmp/.diffconfig.stage1 .config >> tmp/.diffconfig.head
 ./scripts/config/conf --defconfig=tmp/.diffconfig.head -w tmp/.diffconfig.stage2 Config.in >/dev/null
